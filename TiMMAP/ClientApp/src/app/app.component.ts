00d49c13757d22b74050d58abd1c2fe046f23525
jtanksley <jtanksley06@gmail.com>
2019-10-01 15:56:06 -0700

    added styles to M UI

--- TiMMAP/ClientApp/src/app/app.component.ts
+++ TiMMAP/ClientApp/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { GridDataResult, DataStateChangeEvent } from '@progress/kendo-angular-grid';
 import { ColumnsComponent } from './columns/columns.component';
 import { PlaceholderTableComponent } from './placeholder-table/placeholder-table.component';
+import { sharedStylesheetJitUrl } from '@angular/compiler';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
